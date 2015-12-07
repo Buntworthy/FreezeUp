71d9d2a18c689cbfcdb9c856efebf6a7a7f7f35b
Buntworthy <justinpinkney@gmail.com>
2015-12-07 15:46:23 +0000

    Basic camera picture intent

--- app/src/main/java/com/cutsquash/freezeup/EditActivityFragment.java
+++ app/src/main/java/com/cutsquash/freezeup/EditActivityFragment.java
@@ -1,6 +1,11 @@
 package com.cutsquash.freezeup;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,8 +16,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 import java.util.zip.Inflater;
 
 /**
@@ -21,7 +31,11 @@ import java.util.zip.Inflater;
 public class EditActivityFragment extends Fragment implements ItemViewer {
 
     public static final String TAG = EditActivityFragment.class.getSimpleName();
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final String TEMP_IMAGE_FILE = "temp_image.jpg";
+
     private Item mItem;
+    private Uri mfileUri;
 
     public EditActivityFragment() {
     }
@@ -30,8 +44,59 @@ public class EditActivityFragment extends Fragment implements ItemViewer {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_edit, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_edit, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.edit_image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create Intent to take a picture and return control to the calling application
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+                // Save the file to a temporary location in external storage, delete this when we
+                // associate the file with an item
+                File file = new File(
+                        getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                        TEMP_IMAGE_FILE);
+
+                // If there is already a temporary file delete it so we can store a new one
+                if (file.exists()) {
+                    Log.d(TAG, "Deleting existing temporary image file");
+                    file.delete();
+                }
+
+                // Use a Camera capture intent
+                mfileUri = Uri.fromFile(file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mfileUri);
+
+                // start the image capture Intent
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        return  rootView;
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            Log.d(TAG, "Got a result");
+            if (resultCode == Activity.RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Toast.makeText(getActivity(), "Got intent", Toast.LENGTH_LONG).show();
+
+                ImageView imageView = (ImageView) getView().findViewById(R.id.edit_image);
+
+                // Load image with picasso (invalidate to avoid chacheing problems)
+                Picasso.with(getActivity()).invalidate(mfileUri);
+                Picasso.with(getActivity()).load(mfileUri).resize(200, 200)
+                        .centerCrop().into(imageView);
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }
     }
 
     @Override
@@ -84,6 +149,10 @@ public class EditActivityFragment extends Fragment implements ItemViewer {
 
         EditText quantityView = (EditText) rootView.findViewById(R.id.edit_quantity);
         quantityView.setText(item.getQuantityString());
+
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.edit_image);
+        Picasso.with(getActivity()).load(R.drawable.placeholder).resize(200, 200)
+                .centerCrop().into(imageView);
     }
 
     public void save() {

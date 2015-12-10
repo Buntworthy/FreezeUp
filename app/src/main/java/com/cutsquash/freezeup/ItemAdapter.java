2a5c79822f396bc11ee3233b0d3cb8b8c41169d5
Buntworthy <justinpinkney@gmail.com>
2015-12-10 21:46:37 +0000

    Item layout

--- app/src/main/java/com/cutsquash/freezeup/ItemAdapter.java
+++ app/src/main/java/com/cutsquash/freezeup/ItemAdapter.java
@@ -2,14 +2,19 @@ package com.cutsquash.freezeup;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cutsquash.freezeup.data.Contract;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -45,8 +50,18 @@ public class ItemAdapter extends CursorAdapter {
         dateView.setText(itemDate);
 
         String itemImage = cursor.getString(cursor.getColumnIndex(Contract.COL_IMAGE));
-        TextView imageView = (TextView) view.findViewById(R.id.item_image);
-        imageView.setText(itemImage);
+        ImageView imageView = (ImageView) view.findViewById(R.id.item_image);
+
+        File imageFile = new File(
+                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                itemImage);
+        if (imageFile.exists()) {
+            Picasso.with(context).load(imageFile).resize(200, 200)
+                    .centerCrop().into(imageView);
+        } else {
+            Picasso.with(context).load(R.drawable.placeholder).resize(200, 200)
+                    .centerCrop().into(imageView);
+        }
 
         String itemQantity = Integer.toString(cursor.getInt(cursor.getColumnIndex(Contract.COL_QUANTITY)));
         TextView quantityView = (TextView) view.findViewById(R.id.item_quantity);
